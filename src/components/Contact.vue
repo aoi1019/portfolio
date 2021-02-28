e9ef69566a9cb0c1f11be1c0520250623a34b4dd
前田蒼生 <aoi1019m@gmail.com>
2021-02-28 15:45:55 +0900

    update: contactページ完了

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="section">
     <h2 class="section-ttl">Contact</h2>
+    <p class="sub-ttl">お問い合わせ</p>
+    <div class="contact-field">
+      <div class="contact-des">
+        個人開発で得た有益な情報を発信しています。<br>ぜひフォローお願いします！
+      </div>
+      <div class="contact-sns">
+        <div class="sns">
+         <button class="btn twitter-btn sns-btn" onclick="location.href='https://twitter.com/aoi_programming'" ><i class="fab fa-twitter icon"></i>Twitter</button>
+         <button class="btn github-btn sns-btn" onclick="location.href='https://github.com/aoi1019'"><i class="fab fa-github icon"></i>GitHub</button>
+         <button class="btn g-mail-btn sns-btn" onclick="location.href='mailto:yaoi1019m@gmail.com'"><i class="fas fa-envelope icon"></i>Gmail</button>
+         <button class="btn qiita-btn sns-btn" onclick="location.href='https://qiita.com/aoi1019m'"><i class="fas fa-pen-square icon"></i>Qiita</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>

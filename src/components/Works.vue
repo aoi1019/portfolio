4a244bee8488c8899ae9c9ccd662569e65eac62d
前田蒼生 <aoi1019m@gmail.com>
2021-02-28 16:53:57 +0900

    update: worksページ

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="section">
     <h2 class="section-ttl">Works</h2>
+    <p class="sub-ttl">実績・成果物</p>
+    <div class="works-field">
+      <div class="column">
+        <div class="column-name">FindApps</div>
+        <div class="column-img">
+          <img src="../assets/images/find-apps.jpg" class="_img">
+        </div>
+      </div>
+      <div class="column">
+        <div class="column-name">SPOSHIRU</div>
+        <div class="column-img">
+          <img src="../assets/images/health_support_equipment.jpg" class="_img">
+        </div>
+      </div>
+      <div class="column">
+        <div class="column-name">AOIBLOG</div>
+        <div class="column-img">
+          <img src="../assets/images/aoiblog.png" class="_img">
+        </div>
+      </div>
+      <div class="column">
+        <div class="column-name">Qiita記事</div>
+        <div class="column-img">
+          <img src="../assets/images/qiita.png" class="_img">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
